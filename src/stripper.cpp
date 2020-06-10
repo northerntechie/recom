e5655d9381fdd47c7f6399a2d52189d2f0cbfffb
northerntechie <todd.saharchuk@gmail.com>
2020-06-09 18:31:27 -0700

    Initial commit - hello world.

---
+++ src/stripper.cpp
@@ -0,0 +1,20 @@
+/**
+ * stripper utility to strip code files of comments or other
+ * unwanted syntax.  Uses docopts as a dependency.
+ *
+ * Author: Todd Saharchuk
+ * License: MIT
+ *
+ * Copyright 2020 (c) Todd Saharchuk
+ */
+#include <fstream>
+#include <iostream>
+#include "docopt/docopt.h"
+
+int main()
+{
+    std::cout << "Hello world!\n";
+    
+    return EXIT_SUCCESS;
+}
+
